ca483ce2fa2e38fc62b5a4621d7db805aaad9c8b
RealTheBeastBoss <cumbers04@gmail.com>
2023-07-14 17:45:34 +0100

    Added Ore Generation

--- src/main/java/com/thebeastboss/thebeastbossmod/TBBMod.java
+++ src/main/java/com/thebeastboss/thebeastbossmod/TBBMod.java
@@ -5,19 +5,26 @@ import com.thebeastboss.thebeastbossmod.enchantment.WitheringEnchantment;
 import com.thebeastboss.thebeastbossmod.item.*;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.feature.PlacedFeature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TBBMod implements ModInitializer {
 	public static final String MOD_ID = "thebeastbossmod";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final RegistryKey<PlacedFeature> BEASTBOSS_ORE_PLACED_KEY = RegistryKey.of(RegistryKeys.PLACED_FEATURE, new Identifier(MOD_ID, "ore_beastboss"));
 	// TheBeastBoss Mod Items:
 	public static final Item BEASTBOSS_GEM =  Registry.register(Registries.ITEM, new Identifier(MOD_ID, "beastboss_gem"),
 			new Item(new FabricItemSettings()));
@@ -45,5 +52,6 @@ public class TBBMod implements ModInitializer {
 	public void onInitialize() {
 		ModBlocks.registerModBlocks();
 		ModItemGroups.registerItemGroups();
+		BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, BEASTBOSS_ORE_PLACED_KEY);
 	}
 }
